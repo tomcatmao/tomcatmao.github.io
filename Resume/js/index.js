3b7a8b0d8e1e54f7cf6772078cfb64d2f82ff30e
tomcatmao <1071952045@qq.com>
2017-05-18 23:18:39 +0800

    all

---
+++ Resume/js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+	$('.img1').css({'opacity':'1','transform':'rotate(360deg) scale(1)'});
+		$('.img2').css({'opacity':'1','transform':'scale(1)'});
+		$('.head_text').css('opacity','1');
+})
+		
